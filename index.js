a6697e3208725533f54d8265a907e0da39e0c931
Aman Gupta <aman06031999@gmail.com>
2020-05-25 17:51:23 +0530

    adding local usthentication with cookie-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const db=require("./config/mongodb_config");
+const cookie=require("cookie-parser");
 const port=8000;
 const app=express();
 const layout=require("express-ejs-layouts");
@@ -7,21 +8,24 @@ const layout=require("express-ejs-layouts");
 // setting in app the view engine as ejs
 app.set("view engine","ejs");
 app.set("views","views");
+// setting up layout 
+app.set("layout extractScripts",true);
+app.set("layout extractStyles",true);
 
-app.set("layout extractScripts");
-app.set("layout extractStyles");
-
+// using layout middleware
 app.use(layout);
+// using cookie-parser middleware
+app.use(cookie());
 
 
 // setting directory for static files in middleware
-
 app.use(express.static("./assets"));
 app.get("/",function(req,res,next){
+    res.cookie("hi","yo");
     res.render("home");
 });
 
-
+// setting up server
 app.listen(port,function(err){
     if(err)
     {
