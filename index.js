45d382b630c50192326786f7536b3462eb28bcd7
Aman Gupta <aman06031999@gmail.com>
2020-05-25 17:11:32 +0530

    added view engine layouts header and footer and connected to database

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express=require("express");
+const db=require("./config/mongodb_config");
 const port=8000;
 const app=express();
+const layout=require("express-ejs-layouts");
 
+// setting in app the view engine as ejs
+app.set("view engine","ejs");
+app.set("views","views");
+
+app.set("layout extractScripts");
+app.set("layout extractStyles");
+
+app.use(layout);
+
+
+// setting directory for static files in middleware
+
+app.use(express.static("./assets"));
 app.get("/",function(req,res,next){
-    res.send("<h1>hi this is home</h1>");
+    res.render("home");
 });
 
 
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+const schema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true
+    }
+},
+{
+    timestamp:true
+});
